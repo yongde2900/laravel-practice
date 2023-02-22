fbcc68d54a89cbd9732b36750258c25c8ed2c904
yongde2900 <com414141@gmail.com>
2023-02-22 17:01:21 +0800

    done

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
     use HasFactory;
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+    protected $fillable = [
+        'message',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
